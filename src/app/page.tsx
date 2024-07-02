b966279aeb85345da36a68c800d2cbd74d91bbfa
MinSung KIM <same0219@naver.com>
2024-07-02 10:36:43 +0900

    ui modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Suspense } from "react";
 import ProductList from "@/components/ProductList";
 import Slider from "@/components/Slider";
 import Link from "next/link";
-import { BsFire } from "react-icons/bs";
+import Image from "next/image";
 
 const HomePage = async () => {
   return (
@@ -26,7 +26,7 @@ const HomePage = async () => {
           <h1 className="text-2xl font-semibold font-gothic">
             최근 올라온 상품
           </h1>
-          <Link href="/new">
+          <Link href="/list">
             <div className="text-sm text-gray-500 font-bold mr-16">
               더보기 &gt;
             </div>
@@ -42,10 +42,16 @@ const HomePage = async () => {
       <div className="mt-24 px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
         <div className="flex justify-between items-center mb-4">
           <h1 className="text-2xl font-semibold flex items-center font-gothic">
-            <BsFire className="text-red-500 mr-2" />
+            <Image
+              src="/images/flame.gif"
+              alt="Fire Gif"
+              width={24} // 원하는 크기로 설정
+              height={24} // 원하는 크기로 설정
+              className="mr-2"
+            />
             인기 상품
           </h1>
-          <Link href="/popular">
+          <Link href="/list">
             <div className="text-sm text-gray-500 font-bold mr-16">
               더보기 &gt;
             </div>
