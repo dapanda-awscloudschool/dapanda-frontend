06da25bb5997a1897c7b0668f4677fe5ce5dd2b6
MinSung KIM <same0219@naver.com>
2024-07-18 14:47:13 +0900

    bidcheckout

--- src/components/BID/action.tsx
+++ src/components/BID/action.tsx
@@ -1,7 +1,4 @@
 "use server";
-//import * as amqp from "amqplib";
-//import moment from "moment";
-
 const API_URL = process.env.API_URL_SPRING;
 
 export async function BidRequest(formData: FormData) {
@@ -11,7 +8,7 @@ export async function BidRequest(formData: FormData) {
       body: formData,
     });
 
-    if (response.status === 200) {
+    if (response.ok) {
       const text = await response.text();
       console.log("BidRequest response text:", text); // 응답 텍스트 확인
       return text; // 반환값이 올바른 ID인지 확인
@@ -35,16 +32,17 @@ export async function CheckRequest(id: string) {
     const responseText = await response.text();
     console.log(`CheckRequest response text: ${responseText}`);
 
-    if (response.status === 200) {
+    if (response.ok) {
       const res = JSON.parse(responseText);
       console.log("CheckRequest response json:", res); // 응답 JSON 로그 추가
       return res;
     } else {
       console.error("CheckRequest failed with status:", response.status);
+      console.log(`Response text on failure: ${responseText}`);
       return "error";
     }
   } catch (error) {
-    console.error("Failed to submit bid:", error);
+    console.error("Failed to check bid:", error);
     return "error";
   }
 }
